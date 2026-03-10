c7edcf0d71bf4579ca72b1b5dfe10f91f5ba8f60
Rohit Roy <rohitroy0396@gmail.com>
2026-03-10 14:49:40 +0600

    Add all lab tasks

---
+++ javascript/practice.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+JS Class
+        </title>
+    </head>
+ 
+    <body>
+        <center>
+  <h1>Welcome to JS Class</h1>
+       </center>
+     
+ 
+      <script>
+console.log("Hello world");
+ 
+//alert("Hello I am from alert");
+ 
+var name="Mr.Kaisha";
+var age=40;
+var is_human=true;
+var x;
+var y=null;
+name2="Mr. Rahim";
+let name3=80.00000007;
+ 
+ 
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof is_human);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof name2);
+console.log(typeof name3);
+ 
+var num1=40;
+var num2=60;
+ 
+console.log("the sum is : ", num1+num2);
+console.log("the sub is : ", num1-num2);
+console.log("the mul is : ", num1*num2);
+console.log("the div is : ", num1/num2);
+ 
+      </script>
+ 
+    </body>
+</html> 
---
+++ javascript/practice2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+ 
+<html>
+ 
+<body>
+ 
+<h1 id="new" > "This is the JS Intro class" </h1>
+ 
+<h2 id="new2">" "</h2>
+ 
+<p id="para">  Today we will learn about Dom and form validation </p>
+ 
+<button onclick="greet()"> click me </button>
+ 
+<script>
+ 
+document.getElementById("new").innerHTML="Welcome to AIUB";
+document.getElementById("new2").innerHTML="This the DOM Class";
+ 
+var para=document.getElementById("para");
+ 
+para.style.color="black";
+para.style.backgroundColor="green";
+para.style.fontWeight="bold";
+ 
+function greet()
+{
+    alert("Hello World")
+}
+ 
+</script>
+</body>
+ 
+ 
+</html>
